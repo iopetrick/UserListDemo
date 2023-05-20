1e093a98bcc4fe7bd9e3520eb020e1f1a0b6657b
Pratik Prajapati <pratikprajapatisci@gmail.com>
2023-05-20 14:01:08 +0530

    Initial Commit

---
+++ UserListDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserListDemo
+//
+//  Created by Pratikkumar Prajapati on 20/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
